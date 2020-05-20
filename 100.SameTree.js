6e72e73c53596ade10fdfbdef412ecea323053c7
Elaine Wang <elainewang.cal@gmail.com>
2020-05-19 17:55:55 -0700

    Add recursive soln

--- 100.SameTree.js
+++ 100.SameTree.js
@@ -36,3 +36,15 @@ var isSameTree = function(p, q) {
 
 // time: O(n)
 // space: O(n)
+
+
+// recursive solution:
+var isSameTree = function(p, q) {
+    if (p === null && q === null) return true;
+    if (p === null || q === null) return false;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// time: O(n)
+// space: O(lg n) for balanced tree, O(n) for completely unbalanced tree
